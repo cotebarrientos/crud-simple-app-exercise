9943db7302856490e7b4b21f8453703f1ab3e177
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-10-14 17:43:08 +0100

    Add task listing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,15 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Tasks List</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Task Name</span>
-              <button className="btn btn-danger btn-sm float-end mx-2">Delete</button>
-              <button className="btn btn-warning btn-sm float-end">Edit</button>
-            </li>
+            {/* Render every task  */
+              myTasks.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">{item.myTask}</span>
+                  <button className="btn btn-danger btn-sm float-end mx-2">Delete</button>
+                  <button className="btn btn-warning btn-sm float-end">Edit</button>
+                </li>
+              ))
+            }
           </ul>
         </div>
         <div className="col-4">
